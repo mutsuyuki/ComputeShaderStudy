d434d3a2bccaf21a1720f342f05d3fdc11320c0f
mutsuyuki <muttuwo.mob@gmail.com>
2020-05-02 06:48:01 +0900

    particle position initialize at run

--- Assets/_Practice/04_MoveByNoises/UpdatePositionsByNoises.cs
+++ Assets/_Practice/04_MoveByNoises/UpdatePositionsByNoises.cs
@@ -1,11 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
 
 public class UpdatePositionsByNoises : MonoBehaviour {
     [SerializeField] private ComputeShader updatePositionsShader;
-    [SerializeField] private RenderTexture initialPositions; // 初期位置のテクスチャ
     [SerializeField] private RenderTexture currentPositions; // 出力先のテクスチャ
 
     struct KernelInfo {
@@ -17,23 +15,25 @@ public class UpdatePositionsByNoises : MonoBehaviour {
         public RenderTexture newPositions;
     }
 
+    private KernelInfo initializeKernel;
     private List<KernelInfo> kernelInfos = new List<KernelInfo>();
 
     void Start() {
-        kernelInfos.Add(initShader(updatePositionsShader, initialPositions, "MoveByFBMNoise"));
+        initializeKernel = initShader(updatePositionsShader, currentPositions, "SetInitialPositions");
+        kernelInfos.Add(initShader(updatePositionsShader, currentPositions, "MoveByFBMNoise"));
     }
 
-    private static KernelInfo initShader(ComputeShader shader, RenderTexture initialPositions, string kernelName) {
+    private static KernelInfo initShader(ComputeShader shader, RenderTexture positionTexture, string kernelName) {
         KernelInfo kernelInfo = new KernelInfo();
 
-        if (initialPositions.format != RenderTextureFormat.ARGBFloat) {
-            Debug.LogError(initialPositions.name + "は書き込み可能なテクスチャではないようです。");
+        if (positionTexture.format != RenderTextureFormat.ARGBFloat) {
+            Debug.LogError(positionTexture.name + "は書き込み可能なテクスチャではないようです。");
         }
 
         // oldPosition用のテクスチャをinitialPositionに合わせて生成
-        kernelInfo.oldPositions = new RenderTexture(initialPositions.width, initialPositions.height, 1, initialPositions.format);
+        kernelInfo.oldPositions = new RenderTexture(positionTexture.width, positionTexture.height, 1, positionTexture.format);
         kernelInfo.oldPositions.enableRandomWrite = true;
-        Graphics.CopyTexture(initialPositions, kernelInfo.oldPositions);
+        Graphics.CopyTexture(positionTexture, kernelInfo.oldPositions);
 
         // ComputeShaderにテクスチャをセット
         kernelInfo.kernelIndex = shader.FindKernel(kernelName);
@@ -48,21 +48,34 @@ public class UpdatePositionsByNoises : MonoBehaviour {
         float threadGroupSizeX = (float) kernelInfo.oldPositions.width / kernelInfo.threadSizeX;
         float threadGroupSizeY = (float) kernelInfo.oldPositions.height / kernelInfo.threadSizeY;
         if (threadGroupSizeX % 1 != 0 || threadGroupSizeY % 1 != 0) {
-            Debug.LogError(initialPositions.name + "はスレッドグループ数が整数にならないので、テクスチャサイズを変えてください。");
+            Debug.LogError(positionTexture.name + "はスレッドグループ数が整数にならないので、テクスチャサイズを変えてください。");
         }
 
         // newPosition用のテクスチャをinitialPositionに合わせて生成
-        kernelInfo.newPositions = new RenderTexture(initialPositions.width, initialPositions.height, 1, initialPositions.format);
+        kernelInfo.newPositions = new RenderTexture(positionTexture.width, positionTexture.height, 1, positionTexture.format);
         kernelInfo.newPositions.enableRandomWrite = true;
-        Graphics.CopyTexture(initialPositions, kernelInfo.newPositions);
+        Graphics.CopyTexture(positionTexture, kernelInfo.newPositions);
 
         return kernelInfo;
     }
 
     void Update() {
-        // Domain Warp Noise -------------------
+        
         var currnetKernel = kernelInfos[0];
-
+        
+        // initialize per period -------------------
+        if (Time.frameCount % 2000 == 1) {
+            updatePositionsShader.SetTexture(initializeKernel.kernelIndex, "newPositions", initializeKernel.newPositions);
+            updatePositionsShader.Dispatch(
+                initializeKernel.kernelIndex,
+                initializeKernel.newPositions.width / (int) initializeKernel.threadSizeX,
+                initializeKernel.newPositions.height / (int) initializeKernel.threadSizeY,
+                1
+            );
+            Graphics.CopyTexture(initializeKernel.newPositions, currnetKernel.newPositions);
+        }
+        
+        // move by noise -------------------
         Graphics.CopyTexture(currnetKernel.newPositions, currnetKernel.oldPositions);
         updatePositionsShader.SetTexture(currnetKernel.kernelIndex, "oldPositions", currnetKernel.oldPositions);
         updatePositionsShader.SetTexture(currnetKernel.kernelIndex, "newPositions", currnetKernel.newPositions);
